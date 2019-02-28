864d49f74590251dedc4d95f953240d4e95dcdfe
MVC <MVC@MVC-PC>
2019-02-28 18:53:46 +0330

    five

---
+++ MyApp/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyApp.Models;
+using System.Web.Security;
+using MyApp.Classes;
+
+namespace MyApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        DatabaseContext db = new DatabaseContext();
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel register)
+        {
+            if (ModelState.IsValid)
+            {
+                string haspass = FormsAuthentication.HashPasswordForStoringInConfigFile(register.Password, "MD5");
+                Random rand = new Random();
+                int mycode = rand.Next(100000, 900000);
+
+
+                if (!db.Users.Any(u => u.Mobile == register.Mobile))
+                {
+                    User user = new User()
+
+                    {
+
+                        Mobile = register.Mobile,
+                        Password = haspass,
+                        RoleId = db.Roles.Max(r => r.Id),
+                        CodeNumber = mycode.ToString()
+                };
+                    db.Users.Add(user);
+                    db.SaveChanges();
+
+                    SmsSender sms = new SmsSender();
+                    sms.Send(register.Mobile, "ثبت نام شما در فروشگاه انجام شد" + Environment.NewLine + "کد فعال سازی شما " + mycode.ToString());
+                    return RedirectToAction("");
+                }
+                
+            else
+            {
+                    ModelState.AddModelError("Mobile", "شما فبلا ثبت نام کردید");
+
+            }
+        }
+            return View(register);
+    }
+}
+}
---
+++ MyApp/Models/AccountViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+
+namespace MyApp.Models
+{
+    public class RegisterViewModel
+    {
+        [Display(Name = "شماره همراه")]
+        [Required(ErrorMessage = "مقداری برای {0} قرار دهید")]
+        [MaxLength(11, ErrorMessage = "نباید بیشتر از {1} کارکتر باشد")]
+        public string Mobile { get; set; }
+
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = "مقداری برای {0} قرار دهید")]
+        [MaxLength(80, ErrorMessage = "نباید بیشتر از {1} کارکتر باشد")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = " تکرار کلمه عبور")]
+        [Required(ErrorMessage = "مقداری برای {0} قرار دهید")]
+        [MaxLength(80, ErrorMessage = "نباید بیشتر از {1} کارکتر باشد")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage ="کلمه عبور همخانی ندارد")]
+        public string RePassword { get; set; }
+    }
+}
