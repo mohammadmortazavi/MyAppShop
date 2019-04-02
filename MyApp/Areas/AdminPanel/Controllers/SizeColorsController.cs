19b91986d78a76394e948c4f7c50ca51891d0e54
MVC <MVC@MVC-PC>
2019-04-03 00:15:42 +0430

    30

--- MyApp/Areas/AdminPanel/Controllers/SizeColorsController.cs
+++ MyApp/Areas/AdminPanel/Controllers/SizeColorsController.cs
@@ -15,21 +15,22 @@ namespace MyApp.Areas.AdminPanel.Controllers
         private DatabaseContext db = new DatabaseContext();
 
         // GET: AdminPanel/SizeColors
-        public ActionResult Index()
+        public ActionResult Index(int? id)
         {
-            var sizeColors = db.SizeColors.Include(s => s.Colors).Include(s => s.Prodcuts).Include(s => s.Sizes);
+            var sizeColors = db.SizeColors.Where(p=>p.ProductId==id).Include(s => s.Colors).Include(s => s.Prodcuts).Include(s => s.Sizes);
+            ViewBag.myid = id;
             return View(sizeColors.ToList());
         }
 
         
 
         // GET: AdminPanel/SizeColors/Create
-        public ActionResult Create()
+        public ActionResult Create(int ? id)
         {
             ViewBag.ColorId = new SelectList(db.Colors, "Id", "NameColor");
-            ViewBag.ProductId = new SelectList(db.Products, "Id", "Name");
+            ViewBag.ProductId = new SelectList(db.Products.Where(p=>p.Id==id).ToList(), "Id", "Name");
             ViewBag.SizeId = new SelectList(db.Sizes, "Id", "NameSize");
-            return View();
+            return PartialView();
         }
 
         // POST: AdminPanel/SizeColors/Create
@@ -37,19 +38,19 @@ namespace MyApp.Areas.AdminPanel.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,SizeId,ColorId,ProductId")] SizeColor sizeColor)
+        public ActionResult Create([Bind(Include = "Id,SizeId,ColorId,ProductId")] SizeColor sizeColor ,int? id)
         {
             if (ModelState.IsValid)
             {
                 db.SizeColors.Add(sizeColor);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return Redirect("/AdminPanel/SizeColors/Index/" + id);
             }
 
             ViewBag.ColorId = new SelectList(db.Colors, "Id", "NameColor", sizeColor.ColorId);
-            ViewBag.ProductId = new SelectList(db.Products, "Id", "Name", sizeColor.ProductId);
+            ViewBag.ProductId = new SelectList(db.Products.Where(p => p.Id == id).ToList(), "Id", "Name", sizeColor.ProductId);
             ViewBag.SizeId = new SelectList(db.Sizes, "Id", "NameSize", sizeColor.SizeId);
-            return View(sizeColor);
+            return PartialView(sizeColor);
         }
 
         // GET: AdminPanel/SizeColors/Edit/5
@@ -65,7 +66,7 @@ namespace MyApp.Areas.AdminPanel.Controllers
                 return HttpNotFound();
             }
             ViewBag.ColorId = new SelectList(db.Colors, "Id", "NameColor", sizeColor.ColorId);
-            ViewBag.ProductId = new SelectList(db.Products, "Id", "Name", sizeColor.ProductId);
+            ViewBag.ProductId = new SelectList(db.Products.Where(p=>p.Id==sizeColor.ProductId), "Id", "Name", sizeColor.ProductId);
             ViewBag.SizeId = new SelectList(db.Sizes, "Id", "NameSize", sizeColor.SizeId);
             return View(sizeColor);
         }
@@ -75,27 +76,28 @@ namespace MyApp.Areas.AdminPanel.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,SizeId,ColorId,ProductId")] SizeColor sizeColor)
+        public ActionResult Edit([Bind(Include = "Id,SizeId,ColorId,ProductId")] SizeColor sizeColor,int? id)
         {
             if (ModelState.IsValid)
             {
                 db.Entry(sizeColor).State = EntityState.Modified;
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                var sizecolor = db.SizeColors.Find(id);
+                return Redirect("/adminPanel/SizeColors/Index/"+ sizeColor.ProductId);
             }
             ViewBag.ColorId = new SelectList(db.Colors, "Id", "NameColor", sizeColor.ColorId);
             ViewBag.ProductId = new SelectList(db.Products, "Id", "Name", sizeColor.ProductId);
             ViewBag.SizeId = new SelectList(db.Sizes, "Id", "NameSize", sizeColor.SizeId);
-            return View(sizeColor);
+            return PartialView(sizeColor);
         }
 
         // GET: AdminPanel/SizeColors/Delete/5
-        public ActionResult Delete(int? id)
+        public ActionResult Delete(int? id )
         {
             SizeColor sizeColor = db.SizeColors.Find(id);
             db.SizeColors.Remove(sizeColor);
             db.SaveChanges();
-            return RedirectToAction("Index");
+            return Redirect("/AdminPanel/SizeColors/Index/" + sizeColor.ProductId);
         }
 
      
---
+++ MyApp/Controllers/DetailController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyApp.Models;
+
+namespace MyApp.Controllers
+{
+    public class DetailController : Controller
+    {
+        DatabaseContext db = new DatabaseContext();
+        // GET: Detail
+        public ActionResult Index(int? id)
+        {
+            ViewBag.proId = id;
+            return View();
+        }
+        public  ActionResult Gallery(int? id)
+        {
+            var product = db.Products.Find(id);
+            ViewBag.proname = product.Name;
+            var gallery = db.Galleries.Where(g => g.ProductId == id).ToList();
+            return PartialView(gallery);
+
+        }
+    }
+}
